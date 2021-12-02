7611ac22d3c53a0297cc7a8de4b048d6c4ee0e76
Mahendra <kolatemahendra@gmail.com>
2021-12-02 12:48:41 +0530

    String

---
+++ String/src/Java1/Test.java
@@ -0,0 +1,26 @@
+package Java1;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		
+
+		String a = new String("Velocity");
+		String b = new String("Velocity");
+		String c="Mahendra";
+		System.out.println(c.substring(2));
+		System.out.println(c.lastIndexOf("a"));
+		System.out.println(a);
+		System.out.println(b);
+		boolean result=a.equals(b);
+		System.out.println(result);
+		System.out.println(c.length());
+		System.out.println(a.toLowerCase());
+		System.out.println(b.toUpperCase());
+		System.out.println(a.charAt(6));
+		System.out.println(a.endsWith("ty"));
+		System.out.println(b.startsWith("V")); 
+	}
+
+}
